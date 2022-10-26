19a9ed3c533c836e99ba86ada3bed177b70a6bed
Zoe Gonzales <zoegonzales@HS-C02DQBZWMD6M.local>
2022-10-26 15:35:07 -0600

    initial app w/ github oauth

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default App;
